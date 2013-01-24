10c1f4cf55aeeb3e67cd51ba968581a8293361e1
Kimball Germane <krgermane@gmail.com>
2013-01-24 12:39:02 -0700

    For posterity.

--- cesk/cesk.rkt
+++ cesk/cesk.rkt
@@ -1,45 +1,8 @@
 #lang racket
 
 (require redex/reduction-semantics)
-(require "grammar.rkt" "util.rkt")
-
-(define-language bytecode
-  (e (loc n)
-     (loc-noclr n)
-     (loc-clr n)
-     (loc-box n)
-     (loc-box-noclr n)
-     (loc-box-clr n)
-     
-     (let-one e e)
-     (let-void n e)
-     (let-void-box n e)
-     
-     (boxenv n e)
-     (install-value n e e)
-     (install-value-box n e e)
-     
-     (application e e ...)
-     (seq e e e ...)
-     (branch e e e)
-     (let-rec (l ...) e)
-     (indirect x)
-     (proc-const (τ ...) e)
-     (case-lam l ...)
-     l
-     v)
-  
-  (l (lam (τ ...) (n ...) e))
-  
-  (v number
-     void
-     'variable
-     b)
-  
-  (τ val ref)
-  (n natural)
-  (b #t #f)
-  ((x y) variable))
+(require "../racket-machine/grammar.rkt"
+         "../racket-machine/util.rkt")
 
 (define-extended-language CESK bytecode
   (p (C E S K) error)
@@ -123,6 +86,10 @@
         (where E- (framepop E))
         "install-value-box-cont")))
 
+(define-metafunction
+  CESK)
+  
+
 (define ruless
   (reduction-relation
    CESK
@@ -131,8 +98,33 @@
         (where n ,(length (term (e_1 ...))))
         (where (n_1 ...) (count-up n))
         "application")
-    (application e e ...)
-     (seq e e e ...)
+   (--> ((application e_0 e_1 ...) E S K)
+        (e_0 (push n (framepush E)) S (application-rator e_1 ... K))
+        (where n ,(length (term e_1 ...)))
+        "application-rator")
+   (--> ((clos x) E ((x_0 h_0) ... (x (clos)) (x_i+1 h_i+1) ...) (application-rator K))
+        ((single arg func) (framepop E) S K)
+        "no-arg function")
+   (--> (v E S (application-rator e_1 e_2 ... K))
+        (e_1 (stack-set v n E) S (application 0 e_2 ... K))
+        (where n ,(length (term e_1 e_2 ...)))
+        "check")
+   (--> (v E S (application i e_i+1 e_i+2 ... K))
+        (e_i+1 (stack-set v n E) S (application ,(add1 (term i)) e_i+2 ... K))
+        "arg eval")
+   (--> (v E S (application n K))
+        ((stack-ref n E) (stack-set v n E) S (call n K))
+        "now apply")
+
+   (--> ((seq e_0 e_1 e_r ...) E S K)
+        (e_0 (framepush E) S (seq e_1 e_r ... K))
+        "seq-many")
+   (--> ((seq e_0) E S K)
+        (e_0 E S K)
+        "seq-one")
+   (--> (v E S (seq e_1 e_r ... K))
+        ((seq e_1 e_r ...) (framepop E) S K)
+        "seq-cont")
    (--> ((branch e_c e_t e_f) E S K)
         (e_c (framepush E) S (branch e_t e_f K))
         "branch")
@@ -143,17 +135,17 @@
    (--> (#f E S (branch e_t e_f K))
         (e_f (framepop E) S K)
         "branch-cont-false")
-     (let-rec (l ...) e)
-     (indirect x)
+   (let-rec (l ...) e)
+   (indirect x)
    (--> ((proc-const (τ ...) e) E ((x_i h_i) ...) K)
         ((clos x) E ((x ((clos n () e))) (x_i h_i) ...) K)
         (fresh x)
         "proc-const")
-   (--> ((case-lam l ...) E S K)
+   (--> ((case-lam l ...) E S K))
    (--> ((lam (τ ...) (n ...) e) E ((x_i h_i) ...) K)
         ((clos x) E ((x ((clos n (map n stack-ref E) e))) (x_i h_i) ...) K)
         "lam"
-   ((clos n (u ...) x) ...)
+        ((clos n (u ...) x) ...))))
 
 (define procedure-rules
   (reduction-relation
@@ -174,7 +166,7 @@
         "let-rec")))
 
 ;; hide the 'apply append' in a metafunction 
-(define-metafunction runtime
+#;(define-metafunction runtime
   [(flatten ((any ...) ...))
    (any ... ...)])
 
@@ -308,180 +300,32 @@
 (define clos? 
   (redex-match runtime (clos x)))
 
-(define-extended-language loader bytecode
-  (φ - (n n x))
-  (e any)
-  (H any)
-  (h any)
-  (T any)
-  (l any))
-
-(define-metafunction loader
-  [(load e ((x_0 (proc-const (τ ...) e_b)) ...))
-   (uninit (((ε))) H (concat ((x_0 e_0*) ...) T) (e_*))
-   (where ((e_* e_0* ...) H T (y ...))
-          (load’* ((e -) ((proc-const (τ ...) e_b) -) ...)
-                  (x_0 ...)))])
-
-(define-metafunction loader
-  [(incφ - n) -]
-  [(incφ (n_p n_a x) n) (,(+ (term n) (term n_p)) n_a x)])
-
-(define-metafunction loader
-  [(load-lam-rec (lam (τ_0 ...) (n_0 ... n_i n_i+1 ...) e) n_i (y ...))
-   ; When a closure captures itself multiple times, only the last 
-   ; occurrence is considered a self-reference.
-   ((lam n (n_0 ... n_i n_i+1 ...) x) H (concat ((x e_*)) T) (y_* ...))
-   (where n ,(length (term (τ_0 ...))))
-   (where x (fresh-in (y ...)))
-   (where (e_* H T (y_* ...))
-          (load’ e (,(length (term (n_0 ...))) n x) (x y ...)))
-   (side-condition (not (memq (term n_i) (term (n_i+1 ...)))))]
-  [(load-lam-rec l n_j (y ...)) (load’ l - (y ...))])
-
-(define-metafunction loader
-  [(load-lam-rec* () (y ...)) (() () () (y ...))]
-  [(load-lam-rec* ((l_0 n_0) (l_1 n_1) ...) (y ...))
-   ((l_0* l_1* ...) (concat H_0 H_1) (concat T_0 T_1) (y_** ...))
-   (where (l_0* H_0 T_0 (y_* ...))
-          (load-lam-rec l_0 n_0 (y ...)))
-   (where ((l_1* ...) H_1 T_1 (y_** ...))
-          (load-lam-rec* ((l_1 n_1) ...) (y_* ...)))])
-
-(define-metafunction loader
-  [(load’ (application (loc-noclr n) e_1 ...) (n_p n_a x) (y ...))
-   ((self-app x (loc-noclr n) e_1* ...) H T (y_* ...))
-   (side-condition (= (term n) (+ (term n_p) (length (term (e_1 ...))))))
-   (side-condition (= (term n_a) (length (term (e_1 ...)))))
-   (where ((e_1* ...) H T (y_* ...)) (load’* ((e_1 -) ...) (y ...)))]
-  
-  [(load’ (let-rec (l_0 ...) e) φ (y ...))
-   ((let-rec (l_0* ...) e_*)
-    (concat H_0 H_1) (concat T_0 T_1)
-    (y_** ...))
-   (where (e_* H_0 T_0 (y_* ...)) (load’ e φ (y ...)))
-   (where (n_0 ...) (count-up ,(length (term (l_0 ...)))))
-   (where ((l_0* ...) H_1 T_1 (y_** ...)) 
-          (load-lam-rec* ((l_0 n_0) ...) (y_* ...)))]
-  
-  [(load’ (application e_0 e_1 ...) φ (y ...))
-   ((application e_0* e_1* ...) H T (y_* ...))
-   (where ((e_0* e_1* ...) H T (y_* ...))
-          (load’* ((e_0 -) (e_1 -) ...) (y ...)))]
-  
-  [(load’ (let-one e_r e_b) φ (y ...))
-   ((let-one e_r* e_b*) 
-    (concat H_r H_b) (concat T_r T_b)
-    (y_** ...))
-   (where (e_r* H_r T_r (y_* ...)) (load’ e_r - (y ...)))
-   (where (e_b* H_b T_b (y_** ...)) (load’ e_b (incφ φ 1) (y_* ...)))]
-  
-  [(load’ (let-void n e) φ (y ...))
-   ((let-void n e_*) H T (y_* ...))
-   (where (e_* H T (y_* ...)) (load’ e (incφ φ n) (y ...)))]
-  [(load’ (let-void-box n e) φ (y ...))
-   ((let-void-box n e_*) H T (y_* ...))
-   (where (e_* H T (y_* ...)) (load’ e (incφ φ n) (y ...)))]
-  
-  [(load’ (boxenv n e) φ (y ...))
-   ((boxenv n e_*) H T (y_* ...))
-   (where (e_* H T (y_* ...)) (load’ e φ (y ...)))]
-  
-  [(load’ (install-value n e_r e_b) φ (y ...))
-   ((install-value n e_r* e_b*)
-    (concat H_r H_b) (concat T_r T_b)
-    (y_** ...))
-   (where (e_r* H_r T_r (y_* ...)) (load’ e_r - (y ...)))
-   (where (e_b* H_b T_b (y_** ...)) (load’ e_b φ (y_* ...)))]
-  [(load’ (install-value-box n e_r e_b) φ (y ...))
-   ((install-value-box n e_r* e_b*)
-    (concat H_r H_b) (concat T_r T_b)
-    (y_** ...))
-   (where (e_r* H_r T_r (y_* ...)) (load’ e_r - (y ...)))
-   (where (e_b* H_b T_b (y_** ...)) (load’ e_b φ (y_* ...)))]
-  
-  [(load’ (seq e_0 ... e_n) φ (y ...))
-   ((seq e_0* ... e_n*) 
-    (concat H_0 H_1) (concat T_0 T_1)
-    (y_** ...))
-   (where ((e_0* ...) H_0 T_0 (y_* ...)) (load’* ((e_0 -) ...) (y ...)))
-   (where (e_n* H_1 T_1 (y_** ...)) (load’ e_n φ (y_* ...)))]
-  
-  [(load’ (branch e_c e_t e_f) φ (y ...))
-   ((branch e_c* e_t* e_f*)
-    (concat H_c H_t H_f) (concat T_c T_t T_f )
-    (y_*** ...))
-   (where (e_c* H_c T_c (y_* ...)) (load’ e_c - (y ...)))
-   (where (e_t* H_t T_t (y_** ...)) (load’ e_t φ (y_* ...)))
-   (where (e_f* H_f T_f (y_*** ...)) (load’ e_f φ (y_** ...)))]
-  
-  [(load’ (lam (τ_0 ...) (n_0 ...) e) φ (y ...))
-   ((lam n (n_0 ...) x) 
-    H 
-    (concat ((x e_*)) T)
-    (y_* ...))
-   (where x (fresh-in (y ...)))
-   (where n ,(length (term (τ_0 ...))))
-   (where (e_* H T (y_* ...)) (load’ e - (x y ...)))]
-  
-  [(load’ (proc-const (τ_0 ...) e) φ (y ...))
-   ((clos x) 
-    (concat ((x ((clos n () x_*)))) H) 
-    (concat ((x_* e_*)) T)
-    (y_* ...))
-   (where x (fresh-in (y ...)))
-   (where x_* (fresh-in (x y ...)))
-   (where n ,(length (term (τ_0 ...))))
-   (where (e_* H T (y_* ...)) (load’ e - (x x_* y ...)))]
-  
-  [(load’ (case-lam l_0 ...) φ (y ...))
-   ((case-lam l_0* ...) H T (y_* ...))
-   (where ((l_0* ...) H T (y_* ...))
-          (load’* ((l_0 φ) ...) (y ...)))]
-  
-  [(load’ e φ (y ...)) (e () () (y ...))])
-
-(define-metafunction loader
-  [(load’* () (y ...)) (() () () (y ...))]
-  [(load’* ((e_0 φ_0) (e_1 φ_1) ...) (y ...))
-   ((e_0* e_1* ...) 
-    (concat H_0 H_1) (concat T_0 T_1)
-    (y_** ...))
-   (where (e_0* H_0 T_0 (y_* ...)) 
-          (load’ e_0 φ_0 (y ...)))
-   (where ((e_1* ...) H_1 T_1 (y_** ...))
-          (load’* ((e_1 φ_1) ...) (y_* ...)))])
-
-(define-metafunction loader
-  [(fresh-in (x ...))
-   ,(variable-not-in (term (x ...)) 'x)])
-
 (define-metafunction
-  runtime
+  CESK
   heap-ref : (box x) H -> h
   [(heap-ref (box x_i) ((x_0 h_0) ... (x_i h_i) (x_i+1 h_i+1) ...)) h_i])
 
 (define-metafunction
-  runtime
+  CESK
   heap-set : h (box x) H -> H
   [(heap-set h (box x_i) ((x_0 h_0) ... (x_i h_i) (x_i+1 h_i+1) ...))
    ((x_0 h_0) ... (x_i h) (x_i+1 h_i+1) ...)])
 
 (define-metafunction
-  runtime
+  CESK
   push : (u ...) (u ... s) -> (u ... s)
   [(push (u_0 ...) (u_i ... s))
    (u_0 ... u_i ... s)])
 
 (define-metafunction
-  runtime
+  CESK
   push-uninit : n (u ... s) -> (uninit ... u ... s)
   [(push-uninit 0 S) S]
   [(push-uninit n (u ... s))
    (push-uninit ,(- (term n) (term 1)) (uninit u ... s))])
 
 (define-metafunction
-  runtime
+  CESK
   stack-ref : n S -> u
   [(stack-ref 0 (v u ... s)) v]
   [(stack-ref 0 ((box x) u ... s)) (box x)]
@@ -492,7 +336,17 @@
    (stack-ref n (u ... s))])
 
 (define-metafunction
-  runtime
+  CESK
+  framepush : S -> S
+  [(framepush s) (((s)))])
+
+(define-metafunction
+  CESK
+  framepop : S -> S
+  [(framepop (u_0 ... (u_1 ... (u_2 ... s)))) s])
+
+(define-metafunction
+  CESK
   stack-set : u n S -> S
   [(stack-set u n (u_0 ... u_n u_n+1 ... s))
    (u_0 ... u u_n+1 ... s)
@@ -502,7 +356,7 @@
    (u_0 ... (stack-set u ,(- (term n) (length (term (u_0 ...)))) s))])
 
 (define-metafunction
-  runtime
+  CESK
   stack-set* : (u n) ... S -> S
   [(stack-set* S) S]
   [(stack-set* (u_0 n_0) (u_1 n_1) ... S)
